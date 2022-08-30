87e4e0d5dd1fa07adf8294a275b41d9f375b8da1
alidpr <alideypir@gmail.com>
2022-08-30 17:14:44 +0430

    solve 20

---
+++ 20. Valid Parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let stack = [];
+    let open = {'(': ')', '{': '}', '[': ']'};
+    let close = {')': true, '}': true, ']': true};
+
+    for (let char of s) {
+        if (open[char]) {
+            stack.push(char);
+        } else if (close[char]) {
+            if (open[stack.pop()] !== char) return false;
+        }
+    }
+
+    return stack.length === 0;
+};
+
+console.log(isValid("(){}[]"))
