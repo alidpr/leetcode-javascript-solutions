300673bd193c8d614c2cefd828a9b7f632ee33d7
alidpr <alideypir@gmail.com>
2022-08-30 16:27:26 +0430

    solve roman to integer

---
+++ 13. Roman to Integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    // console.log(s.length)
+    const mapper = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    let total = 0;
+    if (s.length <= 15 && s.length >= 1) { //MCMXCIV
+        for (let i = 0; i < s.length; i++) {
+            if (i >= s.length - 1 || mapper[s[i]] >= mapper[s[i + 1]]) {
+                total = total + mapper[s[i]]
+            } else if (mapper[s[i]] < mapper[s[i + 1]]) {
+                const temp = mapper[s[i + 1]] - mapper[s[i]]
+                total = total + temp
+                i += 1
+            }
+            // console.log(i, s[i], mapper[s[i]], mapper[s[i + 1]], total)
+        }
+        console.log(total)
+        return total
+    } else {
+        return false
+    }
+};
